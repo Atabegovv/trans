ed3a179c80b07d2d3d779523e6f74442d6afb58f
Atabegovv <atabegov98@mail.ru>
2023-03-03 13:04:38 +0300

    product page

---
+++ src/js/01_main.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.product__slider', {
+  navigation:{
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  grabCursor: true,
+  spaceBetween: 30,
+  speed: 1000,
+  // freeMode: true,
+  slidesPerView: 2.2,
+  // breakpoints:{
+  //   1050: {
+  //     slidesPerView: 1.2,
+  //   },
+  // },
+});
